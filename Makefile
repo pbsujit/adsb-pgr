3784533e0d71b8336e89bccc6853c58d0ecc49d8
jagernot <jagernot@00867880-1cf4-31cf-2929-e474e1e1c4bb>
2011-09-04 06:20:11 +0000

    check 4sep

--- Makefile
+++ Makefile
@@ -8,12 +8,12 @@ all:		mds02 mds02pr
 
 mds02:		mds02.o shmsub02.o decsub02.o decbds.o
 		$(CC) $(CFGAG) mds02.o shmsub02.o decsub02.o decbds.o \
-		-lm -o mds02 
+		-lm -o mds02
 		chmod +x ./lookup
-	
+
 mds02.o:	mds02.h mds02.c decsub02.h
 		$(CC) $(CFLAG) -c mds02.c -o mds02.o
-		
+
 shmsub02.o:	shmsub02.c mds02.h
 		$(CC) $(CFLAG) -c shmsub02.c -o shmsub02.o
 
@@ -25,7 +25,7 @@ decbds.o:	decbds.c decbds.h mds02.h
 
 mds02pr:	mds02pr.o shmsub02.o
 		$(CC) $(CFGAG) mds02pr.o shmsub02.o -lm -o mds02pr
-	
+
 mdspr02.o:	mds02pr.cc mds02.h
 		$(CC) $(CFLAG) -c mds02pr.cc -o mds02pr.o
 
--- mds02pr.cc
+++ mds02pr.cc
@@ -1,22 +1,24 @@
 /*
-  Copyright (C) 2011  Meinolf Braeutigam
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-  Meinolf Braeutigam, Kannenbaeckerstr. 14
-  53395 Rheinbach, GERMANY
-  meinolf.braeutigam@arcor.de
+  * This file is part of adsb-pgr.
+  *
+  * adsb-pgr is copyright (c) 2011 Meinolf Braeutigam
+  *
+  * adsb-gr is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * adsb-pgr is distributed in the hope that it will be useful, but WITHOUT
+  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+  * for more details.
+  *
+  * You should have received a copy of the GNU General Public License along
+  * with adsb-pgr.  If not, see <http://www.gnu.org/licenses/>.
+  *
+  * Meinolf Braeutigam, Kannenbaeckerstr. 14
+  * 53395 Rheinbach, GERMANY
+  * meinolf.braeutigam@arcor.de
 */
 
 #include <stdio.h>
@@ -100,14 +102,15 @@ struct state {
   double last_alt; // last altitude
   double fcu_alt; // intended altitude
 
+  // take MAX_DELTA height changes b4 deciding ascending or descending
   int ndelta;
-  double total_delta_alt;
+  double delta_alt;
   static const int MAX_DELTAS = 3;
 
   state () : stat ("unknown") {
     alt = last_alt = fcu_alt = 0;
     ndelta = 0;
-    total_delta_alt = 0;
+    delta_alt = 0;
   }
 
 };
@@ -119,7 +122,8 @@ struct info { // from web lookup
   string type;
   string from;
   string to;
-  int tries;
+
+  int tries; // num times to lookup web b4 giving up
   info () : tries(0) {}
 
 };
@@ -127,7 +131,7 @@ struct info { // from web lookup
 typedef string plane_hex;
 typedef string flight_id;
 
-map<plane_hex, info> plane_db;
+map<plane_hex, info> plane_info;
 map<flight_id, state> plane_state;
 
 string show ("both");
@@ -149,41 +153,40 @@ int p_ref (void)
     if (p->lat != UNKNOWN && p->lon != UNKNOWN && strlen (p->acident) > 3) {
      planes.push_back (aircraft (p));
      aircraft& plane = planes [planes.size() - 1];
-     state& st= plane_state [plane.flight_number];
-     if (st.stat == "unknown") {
-      st.stat = "level";
-      st.last_alt = st.alt = p->alt;
+     state& ps = plane_state [plane.flight_number];
+     if (ps.stat == "unknown") {
+      ps.stat = "level";
+      ps.last_alt = ps.alt = ps.fcu_alt = p->alt;
      } else {
-      st.alt = p->alt;
-      double delta_alt = st.alt - st.last_alt;
-      st.total_delta_alt += delta_alt;
-      if (++st.ndelta >= state::MAX_DELTAS) {
-        delta_alt = st.total_delta_alt / state::MAX_DELTAS;
-        st.ndelta = 0;
-        st.total_delta_alt = 0;
-        if (delta_alt > 0) {
-          st.stat = "ascending";
-        } else if (delta_alt < 0) {
-          st.stat = "descending";
-        }
+      ps.alt = p->alt;
+      double delta_alt = ps.alt - ps.last_alt;
+      ps.delta_alt += delta_alt;
+      if (++ps.ndelta >= state::MAX_DELTAS) {
+        delta_alt = ps.delta_alt / state::MAX_DELTAS;
+        ps.ndelta = ps.delta_alt = 0;
+        if (delta_alt > 0) ps.stat = "ascending"; else if (delta_alt < 0) ps.stat = "descending";
       }
-      st.last_alt = st.alt;
+      ps.last_alt = ps.alt;
     }
 
-    st.fcu_alt = p->bds.fcu_alt_40 - (int) p->bds.fcu_alt_40 % 10;
-    if (st.fcu_alt < 0) st.fcu_alt = 0;
+    // round fcu_alt to nearest 10
+    ps.fcu_alt = p->bds.fcu_alt_40 - (int) p->bds.fcu_alt_40 % 10;
+    if (ps.fcu_alt < 0) ps.fcu_alt = 0;
 
    }
+
   }
 
+  // sort planes closest to observer
   sort (planes.begin(), planes.end(), closest ());
 
+  // print
   for (int i = 0, j = planes.size (), k = 0; i < j; ++i) {
     aircraft& a = planes [i];
-    info& d = plane_db [a.icao];
+    info& d = plane_info [a.icao];
     state& s = plane_state [a.flight_number];
-    if (d.from == "" && d.to == "" && ++d.tries < 3) {
-      static char from[256], to[256], callsign[256], latlon[256];
+    if (d.from == "" && d.to == "" && ++d.tries < 3) { // look web to find flight itinerary for flight number(see ./lookup)
+      static char from [256], to [256], callsign [256], latlon [256];
       sprintf (latlon, " %f %f", a.p->lat, a.p->lon);
       string cmd("./lookup " + string(a.p->acident) + latlon);
       system (cmd.c_str());
@@ -197,10 +200,7 @@ int p_ref (void)
       system ("rm -f out");
     }
 
-    if (show == "both") {
-      if (s.stat == "ascending") printf ("\e[1;35m"); else if (s.stat == "descending") printf ("\e[1;31m");
-      printf ("%03d %8s %6s %6s %4s %05.0f %10s to %05.0f %17s %s -> %s\n", ++k, a.flight_number.c_str(), a.icao.c_str(), d.reg.c_str(), d.type.c_str(), s.alt, s.stat.c_str(), s.fcu_alt, d.callsign.c_str(), d.from.c_str(), d.to.c_str());
-    } else if (show == s.stat || show == "level") {
+    if (show == "both" || show == s.stat || show == "level") {
       if (s.stat == "ascending") printf ("\e[1;35m"); else if (s.stat == "descending") printf ("\e[1;31m");
       printf ("%03d %8s %6s %6s %4s %05.0f %10s to %05.0f %17s %s -> %s\n", ++k, a.flight_number.c_str(), a.icao.c_str(), d.reg.c_str(), d.type.c_str(), s.alt, s.stat.c_str(), s.fcu_alt, d.callsign.c_str(), d.from.c_str(), d.to.c_str());
     }
@@ -213,44 +213,42 @@ int p_ref (void)
  * ------------------------------------------------------------------------ */
 main(int argc, char** argv)
 {
-   if (argc < 2) show = "both"; else show = argv[1];
+  if (argc < 2) show = "both"; else show = argv[1];
 
-   int n,k;
+  int n,k;
 
 /* prepare shared memory access (with params from 'mds02.h') */
 
-   if ((k = get_shm_id()) == 0) exit(1); /* get shared mem id
-             *   from mds02 log file */
-   if ((n = shm_att(k)) != 0)   /* connect to shared memory */
-     {
-    fprintf(stderr,"\nshared memory connection error:\n\t%s\n",
-      strerror(n));
-    exit(1);
-     }
+  if ((k = get_shm_id()) == 0) exit(1); /* get shared mem id*/
+  if ((n = shm_att(k)) != 0)   /* connect to shared memory */ {
+   fprintf(stderr,"\nshared memory connection error:\n\t%s\n",
+   strerror(n));
+   exit(1);
+  }
 
-/* pointer to 'Misc' structure */
+  /* pointer to 'Misc' structure */
 
-   p_mi = p_2_misc();
+  p_mi = p_2_misc();
 
-/* loop until terminated manually by <CTRL>-C */
-
+  // read plane icao, registration & type database
   ifstream dbf ("db.dat");
   while (!dbf.eof()) {
-    string hex, reg, type, k;
+    string hex, reg, type;
     dbf >> hex >> reg >> type;
-    info& d = plane_db[hex];
+    info& d = plane_info [hex];
     d.reg = reg; d.type = type;
   }
 
   char top[] = {"\033[0;0H\033[J"}; /* vt100 'top of clear page' string  */
 
-   while(1)       /* loop forever */
-     {
+  /* loop until terminated manually by <CTRL>-C */
 
+  while(1) /* loop forever */ {
     printf("%s",top);
     printf ("%3s %8s %6s %6s %4s %5s %19s %17s %s\n", "No:", "Flight  ", "ICAO  ", "Regn  ", "Type", "  Alt", "     State", "         Callsign", "From -> To");
     p_ref();
     sleep(1);
-   }
+  }
+
 }
 
