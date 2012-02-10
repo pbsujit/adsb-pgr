ff8791c029ec4ae683fd43a2aca572658cf8c2f2
jagernot <jagernot@00867880-1cf4-31cf-2929-e474e1e1c4bb>
2012-02-10 19:11:48 +0000

    tw

--- trunk/twitter.tcl
+++ trunk/twitter.tcl
@@ -9,6 +9,7 @@ set current_time [clock seconds]
 set delta_time [expr $current_time - $last_time]
 
 if { $delta_time > 20 } {
+
   set twitterok 1
 
   set f [open time w]
@@ -30,9 +31,9 @@ if {$twitterok} {
   set consumer_key qg6FNclfUFj7QgVNlmPr3g
   set consumer_secret qh9TWL80iaxyeq7kjguP759nS1nasEM3iGcAQFbToUk
 
-  set oauth_token 488451779-kSJoLkCD4gsQFrosctFGD7pGZOcyisU1ZEhD8fTF
-  set oauth_token_secret Sx4eZru1uVHoOzjiMnKdpH5xCPBg93Ac9amiyskaE
+  set oauth_token 488703816-NQTamyQdrxvehyQIkNnK5NuCdKT7URlqn9xXmu6m
+  set oauth_token_secret BYERXmLerBjRUvDCVHz1WS1ICbyJ39ehmvpYPyCWZKY
 
-  oauth::query_api http://api.twitter.com/1/statuses/update.json $consumer_key $consumer_secret POST $oauth_token $oauth_token_secret [list status "$flightnum | $caller | $from -> $to | altitude: $alt ft, intent: $intent ft | $reg" lat $lat long $lon]
+  oauth::query_api http://api.twitter.com/1/statuses/update.json $consumer_key $consumer_secret POST $oauth_token $oauth_token_secret [list status "$flightnum | $reg | $caller | $from -> $to | @ $alt feet"]
 
 }
