02f77025a5446a0f351c7aa23943014bb41a2006
jagernot <jagernot@00867880-1cf4-31cf-2929-e474e1e1c4bb>
2012-02-10 14:45:31 +0000

    tw

--- twitter.tcl
+++ twitter.tcl
@@ -6,12 +6,25 @@ set twitterok 0
 
 set current_time [clock seconds]
 
-if { ($current_time - $last_time) > 140 } {
+set delta_time [expr $current_time - $last_time]
+
+if { $delta_time > 20 } {
   set twitterok 1
+
+  set f [open time w]
+    puts $f $current_time
+  close $f
+
+} else {
+  puts "$delta_time is delta"
+}
+
+if {$twitterok} {
+
+  puts "posting twitter";
+
   lappend auto_path /usr/local/lib/tcllib1.14
-
   source oauth.tcl
-
   package require json
 
   set consumer_key qg6FNclfUFj7QgVNlmPr3g
@@ -20,9 +33,6 @@ if { ($current_time - $last_time) > 140 } {
   set oauth_token 488451779-kSJoLkCD4gsQFrosctFGD7pGZOcyisU1ZEhD8fTF
   set oauth_token_secret Sx4eZru1uVHoOzjiMnKdpH5xCPBg93Ac9amiyskaE
 
-  set f [open time w]
-  puts $f $current_time
-  close $f
+  oauth::query_api http://api.twitter.com/1/statuses/update.json $consumer_key $consumer_secret POST $oauth_token $oauth_token_secret [list status "$flightnum | $caller | $from -> $to | altitude: $alt ft, intent: $intent ft | $reg" lat $lat long $lon]
 
 }
-
